d5220760e172f41b6734a73420316361eddb989b
Galaceon <antoniogarcialtr@gmail.com>
2026-04-09 04:48:41 +0200

    conditional way to display or hide components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import BudgetForm from "./components/BudgetForm"
+import BudgetTracker from "./components/BudgetTracker"
 import { useBudget } from "./hooks/useBudget"
+import { useMemo } from "react"
 
 
 function App() {
 
-    const {state, dispatch} = useBudget()
-    console.log(state)
+    const {state} = useBudget()
+
+    const isValidBudget = useMemo(() => state.budget > 0, [state.budget])
 
     return (
         <>
             <header className="bg-blue-600 py-8 max-h-72">
-                <h1 className="uppecase text-center font-black text-4xl text-white">
+                <h1 className="uppercase text-center font-black text-4xl text-white">
                     Planificador de Gastos
                 </h1>
             </header>
 
             <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-                <BudgetForm 
-                
-                />
+                {isValidBudget ? <BudgetTracker /> : <BudgetForm />}
             </div>
         </>
     )
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,10 +1,11 @@
-import type { ChangeEvent } from "react"
-import { useMemo, useState } from "react"
+import { useMemo, useState, type ChangeEvent, type SubmitEvent } from "react"
+import { useBudget } from "../hooks/useBudget"
 
 
 export default function BudgetForm() {
 
     const [budget, setBudget] = useState(0)
+    const {dispatch} = useBudget()
 
     const handleChange = (e : ChangeEvent<HTMLInputElement, HTMLInputElement>) => {
         setBudget(e.target.valueAsNumber)
@@ -13,8 +14,14 @@ export default function BudgetForm() {
         return isNaN(budget) || budget <= 0
     }, [budget])
 
+    const handleSubmit = (e : SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        dispatch({type: "add-budget", payload: {budget}})
+    }
+
     return (
-        <form className="space-y-5">
+        <form className="space-y-5" onSubmit={handleSubmit}>
             <div className="flex flex-col space-y-5">
                 <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">Definir Presupuesto</label>
                 <input
