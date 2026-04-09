c6af6741c5baaf91b5d58cb9613ec7c383a50237
Galaceon <antoniogarcialtr@gmail.com>
2026-04-09 05:52:10 +0200

    creating action to close modal

--- src/reducers/budgetReducer.ts
+++ src/reducers/budgetReducer.ts
@@ -1,7 +1,8 @@
 
 export type BudgetActions = 
     { type: 'add-budget', payload: {budget: number} } |
-    { type: 'show-modal'}
+    { type: 'show-modal'} |
+    { type: 'close-modal'}
 
 export type BudgetState = {
     budget: number
@@ -31,6 +32,13 @@ export const budgetReducer = (
         return {
             ...state,
             modal: true
+        }
+    }
+    if(action.type === 'close-modal') {
+
+        return {
+            ...state,
+            modal: false
         }
     }
 }
