574a0299a618573c6629ef0ece0a96b35bd49b49
Galaceon <antoniogarcialtr@gmail.com>
2026-04-15 18:42:40 +0200

    adding a true interactive circular chart

--- src/components/BudgetTracker.tsx
+++ src/components/BudgetTracker.tsx
@@ -1,15 +1,36 @@
+import {CircularProgressbar, buildStyles} from 'react-circular-progressbar'
+
 import { useBudget } from "../hooks/useBudget";
 import AmountDisplay from "./AmountDisplay";
+import "react-circular-progressbar/dist/styles.css"
 
 
 export default function BudgetTracker() {
 
     const {state, expenseAmount, availableBudget} = useBudget()
 
+    const percentage = +((expenseAmount / state.budget) * 100).toFixed(2)
+
+    console.log(percentage)
+
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
             <div className="flex justify-center">
-                <img src="/grafico.jpg" alt="Gráfica de Gastos" />
+                <CircularProgressbar 
+                    value={percentage}
+                    background
+                    backgroundPadding={6}
+                    styles={buildStyles({
+
+                        backgroundColor: percentage === 100 ? '#dc3939' : '#3b82f6',
+                        textColor: '#E5F5F5',
+                        pathColor: '#E5F5F5',
+                        trailColor: "transparent",
+                        strokeLinecap: "butt",
+                        textSize: 16
+                    })}
+                    text={`${percentage}%`}
+                />
             </div>
 
             <div className="flex flex-col justify-center items-center gap-8">
